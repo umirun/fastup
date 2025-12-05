5b319d68b8a328074fb8e3f2e1fff0ca76de9e3b
Umirunjon <umirun009@gmail.com>
2025-12-05 16:42:14 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
-import { useState } from "react";
-import { Hush , Salo } from './index.jsx';
-
-// ---- ASOSIY APP ----
+import React from "react";
+import Dars from "./Dars";
 
 function App() {
   return (
-    <div className='App'>
-      < Hush />
-      <Salo name="Umrbek" />
-      <Salo name="Umrbek1" />
-      <Salo name="UMrbek2" />
+    <div>
+      <Dars />
     </div>
   );
-}  
-  function ToggleText() {
-    const [text, setText] = useState("4.Salom");
-    return (
-      <div>
-        <h2>{text}</h2>
-        <button
-          onClick={() =>
-            setText(
-              text === "4.Salom"
-                ? "4.Xayr!"
-                : "4.Xayr"
-            )
-          }
-        >
-          Almashtir
-        </button>
-      </div>
-    );
 }
 
 export default App;
+
